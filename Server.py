50e7c1602dc21bee56a7633f1fb58f2df6a279d4
OkeWoke <toamumbo@gmail.com>
2016-02-22 19:51:59 +1300

    Added UserList parsing, and ID prefixes for packets

--- Server.py
+++ Server.py
@@ -1,32 +1,32 @@
 from socket import *
 from threading import *
-import json,sys
+import json,sys,time
 
 if sys.version_info >=(3,0):
         raw_input = input
         
 class Server():
 	def __init__(self):
-		self.port = 44444
+		self.port = 11111
 		self.serverName = "Server: "
-		self.motd = [self.serverName,"You have connected to Oke\'s Server\n"]
+		self.motd = [1,self.serverName,"You have connected to Oke\'s Server\n"]
 		self.connections = []
 		self.s =socket(AF_INET,SOCK_STREAM) #Create Socket Object
 		self.s.bind(('',self.port)) #Bind port to computer
 		self.s.listen(25) #Listens for 25 connections
-
+		self.userList = []
 		commandHandler = Thread(target=self.serverCommand)
 		commandHandler.start()
 		
 		self.acceptConnections()		
 		
 	def acceptConnections(self):
-
 		while True:
 			client,addr = self.s.accept()
 			alias = str(client.recv(1024).decode('utf-8'))#This does not use serverDecode because the server is receiving data that is not in the regular format that is a list.
+			self.userList.append(alias)
 			print("Connection made with "+addr[0]+"\n"+alias+" has connected.")
-
+			
 			client.send(self.serverEncode(self.motd))
 			clientDict= {
 				"alias": alias,
@@ -36,16 +36,19 @@ class Server():
 			self.connections.append(clientDict)
 			clientHandler = Thread(target=self.clientReceive,args=(clientDict,))			
 			clientHandler.start()
-			self.broadcast( self.serverEncode(["" , alias + "  has connected"]))
+			time.sleep(1)
+			self.broadcast([0, self.userList])
+			self.broadcast([1,"", alias + "  has connected"])
 
 	def broadcast(self, data):
+		data = self.serverEncode(data)
 		for clientDict in self.connections:
 			try:
 				clientDict["clientOb"].send(data)
 			except:
 				print("Unable to send data to: "+ clientDict["alias"] + " " + str(clientDict["ip"]))
-				clientDict["clientOb"].close()
-				self.connections.remove(clientDict)
+				#clientDict["clientOb"].close()
+				#self.connections.remove(clientDict)
 
 	def serverEncode(self, data):
 		data = json.dumps(data).encode('utf-8')
@@ -59,14 +62,20 @@ class Server():
 		while True:
 			try:
 				data = clientDict["clientOb"].recv(1024)
-				print(self.serverDecode(data))		
+				data= self.serverDecode(data)
+				print(data)		
 
 			except:
 				print(clientDict["alias"] + " has disconnected")
-				self.broadcast(self.serverEncode(["", clientDict["alias"] +" has disconnected"]))
+				self.broadcast([1,"", clientDict["alias"] +" has disconnected"])
+				for i in self.userList:
+					if i == clientDict["alias"]:
+						self.userList.remove(i)
+				self.broadcast([0, self.userList])
 				break
 			try:
-				self.broadcast(data)
+				if data[0] ==1:
+					self.broadcast(data)
 			except:
 				print("Failed to broacast data to everyone?!?")
 				break
@@ -83,12 +92,11 @@ class Server():
 			print("Unable to remove clientData, possibly removed elsewhere")
 
 	def shutDown(self):
-		self.broadcast(self.serverEncode([self.serverName,"Server Shutting Down..."]))
+		self.broadcast([1,self.serverName,"Server Shutting Down..."])
 		for clientDict in self.connections:
 			clientDict["clientOb"].close()
 		print("shutting down server...")
 		sys.exit()
-		quit()
 
 	def serverCommand(self):
 		while True:
@@ -98,6 +106,8 @@ class Server():
 				self.shutDown()		
 			elif command[0] =="say":
 				msg = command[1:]
-				self.broadcast(self.serverEncode([self.serverName," ".join(msg)]))
+				self.broadcast([1,self.serverName," ".join(msg)])
 						
 server = Server()
+# 1 = string message
+# 0 = update userList
